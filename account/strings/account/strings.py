febaf9b4aee6f08c5008014b9b3c9a5274ad4cde
pektar <amin.pektar@gmail.com>
2018-07-13 13:04:00 +0430

    salam
    salam

---
+++ account/strings/account/strings.py
@@ -0,0 +1,14 @@
+"""
+All string of account app
+"""
+
+############################################
+# --------- STATUS MODEL OBJECT ---------- #
+############################################
+ACTIVE_STATUS = {
+    "status": "ACTIVE",
+    "description": "Your account is active.",
+    "can_login": True
+}
+
+STATUSES = [ACTIVE_STATUS]
