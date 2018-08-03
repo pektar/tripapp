0f75335d2b22530921c9be8786956589d984e29f
pektar <amin.pektar@gmail.com>
2018-08-04 02:30:26 +0430

    change regex for username validator.

--- account/validators.py
+++ account/validators.py
@@ -6,9 +6,10 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class UsernameValidator(validators.RegexValidator):
-    regex = r'^[a-zA-Z]+[a-zA-Z0-9.]{2,}$'
+    regex = r'^[a-zA-Z]{1}[a-zA-Z0-9.]{2,29}$'
     message = _(
         'Enter a valid username. This value may contain only English letters, '
         'numbers, and @/./_ characters.'
+        'username must be at least 3 and at most 30 character.'
     )
     flags = re.ASCII
